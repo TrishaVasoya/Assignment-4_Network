716435c44428684bbd4c55dc09f8cf37aaf32590
trisha <trishavasoya@gmail.com>
2024-04-06 14:08:38 -0400

    Successfully deleted post with ajax and worked with local storage

--- src/posts/static/posts/detail.js
+++ src/posts/static/posts/detail.js
@@ -1,4 +1,5 @@
 console.log('hello world detail')
+console.log(window.location)
 const postBox = document.getElementById('post-box')
 const alertBox = document.getElementById('alert-box')
 const backBtn = document.getElementById('back-btn')
@@ -88,3 +89,24 @@ updateForm.addEventListener('submit', e=>{
 
     })
 })
+
+deleteForm.addEventListener('submit', e=>{
+    e.preventDefault()
+
+    $.ajax({
+        type: 'POST',
+        url: deleteUrl,
+        data:{
+            'csrfmiddlewaretoken': csrf[0].value,
+        },
+        success: function(response){
+            window.location.href = window.location.origin
+            localStorage.setItem('title',titleInput.value)
+        },
+        error: function(error){
+            console.log(error)
+        }
+
+    })
+})
+
